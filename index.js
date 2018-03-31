f7a09ea03c8cdac5b13f5459b5b7872c20b63547
jee <jag918@nyu.edu>
2018-03-31 04:09:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var animal = 'dog'
-animal='cat'
+animal = 'dog'
+var animal='cat'
 function myAnimal() {
   return animal
 }
@@ -10,12 +10,12 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
-
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
